cd99f0b90f24c62cd9087d95dc67e0bf47ef21f0
Cody <8092790+ccccooooddddyyyy@users.noreply.github.com>
2022-06-30 14:11:47 +0700

    Capitalize name of Vietnamese weekdays
    Name of weekdays has to be written in uppercase according to Vietnam Law.

--- src/locale/vi.js
+++ src/locale/vi.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 
 const locale = {
   name: 'vi',
-  weekdays: 'chủ nhật_thứ hai_thứ ba_thứ tư_thứ năm_thứ sáu_thứ bảy'.split('_'),
+  weekdays: 'Chủ Nhật_thứ Hai_thứ Ba_thứ Tư_thứ Năm_thứ Sáu_thứ Bảy'.split('_'),
   months: 'tháng 1_tháng 2_tháng 3_tháng 4_tháng 5_tháng 6_tháng 7_tháng 8_tháng 9_tháng 10_tháng 11_tháng 12'.split('_'),
   weekStart: 1,
   weekdaysShort: 'CN_T2_T3_T4_T5_T6_T7'.split('_'),
